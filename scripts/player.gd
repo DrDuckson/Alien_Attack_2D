3767cc25bde1cf6452120b005d84ef9e801b8e42
DrDuckson <gorealex95@web.de>
2024-05-28 23:17:11 +0200

    Added Player movement, clamp and shoot-function

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,48 @@
 extends CharacterBody2D
 
+var speed = 300
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+func _ready():
+	rocket_scene
+	
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot_rocket()
+
 func _physics_process(delta):
-	velocity = Vector2(100,0)
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+		
 	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	# Begrenzung 1
+	#if global_position.x < 0:
+		#global_position.x = 0
+	#if global_position.x > screen_size.x:
+		#global_position.x = screen_size.x
+	#if global_position.y < 0:
+		#global_position.y = 0;
+	#if global_position.y > screen_size.y:
+		#global_position.y = screen_size.y
+	
+	# Begrenzung 2
+	#global_position.x = clampf(global_position.x, 0, screen_size.x)
+	#global_position.y = clampf(global_position.y, 0, screen_size.y)
+	
+	# Begrenzung 3 = beste performance
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
+func shoot_rocket():
+	#var rocket_scene = preload("res://scenes/rocket.tscn")
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 50
