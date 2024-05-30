3c86e52e11ea10b2de0bafa134a60164c588d3de
DrDuckson <gorealex95@web.de>
2024-05-30 21:55:15 +0200

    Edited "drift"

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,13 @@ var iSpeed = 300
 var iForce = 300
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
+@onready var rocket_container = $RocketContainer 
+# = get_node("RocketContainer")
+
 func _ready():
-	rocket_scene
+	rocket_container = get_node("RocketContainer")
 	
-func _process(delta):
+func _process(deldta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot_rocket()
 
@@ -24,23 +27,18 @@ func _physics_process(delta):
 		
 	
 	if Input.is_action_just_released("move_up"):
-		pass
-		velocity.y = -iSpeed*0.2
+		velocity.y = -iSpeed*0.1
 	if Input.is_action_just_released("move_down"):
-		pass
-		velocity.y = iSpeed*0.2
+		velocity.y = iSpeed*0.1
 	if Input.is_action_just_released("move_left"):
-		pass
-		velocity.x = -iSpeed*0.2
+		velocity.x = -iSpeed*0.1
 	if Input.is_action_just_released("move_right"):
-		pass
-		velocity.x = iSpeed*0.2
+		velocity.x = iSpeed*0.1
 		
 	move_and_slide()	
 	
-	print(global_position)
 	var screen_size = get_viewport_rect().size
-	# Begrenzung 1
+	#Begrenzung 1
 	#if global_position.x < 0:
 		#global_position.x = 0
 	#if global_position.x > screen_size.x:
@@ -58,7 +56,7 @@ func _physics_process(delta):
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
 	
 func shoot_rocket():
-	#var rocket_scene = preload("res://scenes/rocket.tscn")
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 50
