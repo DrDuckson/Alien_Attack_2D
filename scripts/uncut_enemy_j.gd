3d06e58bec8afbf86dd60602eaa67572ab1a4cd1
DrDuckson <gorealex95@web.de>
2024-06-01 23:21:08 +0200

    Added custom signal for dead enemies and function for player collision

--- scripts/uncut_enemy_j.gd
+++ scripts/uncut_enemy_j.gd
@@ -1,20 +1,22 @@
 extends Area2D
 
-@onready var visible_notifier = $EnemyJ_Visible_Notifier
+signal died
 var rng = RandomNumberGenerator.new()
 @onready var speed = rng.randf_range(250, 400)
 
 func _ready():
-	visible_notifier.connect("screen_exited", _on_screen_exited)
-#func _ready():
-	#visible_notifier.connect("screen_exited", _on_screen_exited)
-
-func _on_screen_exited():
-	queue_free()
+	pass
 
 func _process(delta):
 	pass
 
 func _physics_process(delta):
-	# global_position.x = global_position.x + speed*delta 
 	global_position.x -= speed*delta
+
+func die():
+	emit_signal("died")
+	queue_free()
+
+func _on_body_entered(body):
+	body.take_damage()
+	die()
