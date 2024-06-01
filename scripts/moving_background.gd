027443ac34a1f7ec0aff0c0270da7aef444630f8
DrDuckson <gorealex95@web.de>
2024-06-01 23:23:34 +0200

    Cleanup script

--- scripts/moving_background.gd
+++ scripts/moving_background.gd
@@ -1,11 +1,7 @@
 extends ParallaxBackground
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	scroll_base_offset -= Vector2(0.5,0)
