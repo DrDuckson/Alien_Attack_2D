30fd3b162beef7cbd1624ee685881a2bab1f2fa8
DrDuckson <gorealex95@web.de>
2024-06-01 23:22:03 +0200

    Added delete-function for targets and rocket

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -9,9 +9,14 @@ func _ready():
 func _on_screen_exited():
 	queue_free()
 
+func _on_area_entered(area):
+	queue_free()
+	area.die()
+
 func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	# global_position.x = global_position.x + speed*delta 
 	global_position.x += speed*delta
+
+
