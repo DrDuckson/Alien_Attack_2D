fa223082248231a7bc2c5f08c40f294eed452405
DrDuckson <gorealex95@web.de>
2024-06-02 22:17:36 +0200

    Added missed shot signal

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal missed_shot
 @export var speed = 1000
 @onready var visible_notifier = $VisibleNotifier
 
@@ -8,6 +9,7 @@ func _ready():
 
 func _on_screen_exited():
 	queue_free()
+	emit_signal("missed_shot")
 
 func _on_area_entered(area):
 	queue_free()
