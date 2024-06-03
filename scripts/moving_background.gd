60f2bd68a60d37690ba5b693df6476527da083ed
Alex <142669748+DrDuckson@users.noreply.github.com>
2024-06-03 10:44:18 +0200

    Changed moving background to _physics_process(delta):

--- scripts/moving_background.gd
+++ scripts/moving_background.gd
@@ -3,5 +3,8 @@ extends ParallaxBackground
 func _ready():
 	pass
 
-func _process(delta):
+#func _process(delta):
+#	scroll_base_offset -= Vector2(0.5,0)
+
+func _physics_process(delta):
 	scroll_base_offset -= Vector2(0.5,0)
